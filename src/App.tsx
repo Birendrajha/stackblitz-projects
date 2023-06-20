f6c0291d6fbf74211f7227f2ebf4567ea5200e4d
Birendrajha <63343940+Birendrajha@users.noreply.github.com>
2023-06-20 17:42:25 +0530

    6 files modified

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,26 @@ import {
   getData,
 } from './features/reducers';
 import NameComp from './components/name.comp';
+import EmailComp from './components/email.comp';
+import MobileNoComp from './components/mobile.comp';
+import GenderComp from './components/gender.comp';
+import PasswordComp from './components/password.comp';
 export default function App() {
+  const data = useSelector((c) => {
+    console.log(c.show);
+    return c.show;
+  });
+
   return (
     <div>
       <h1>React Redux</h1>
-      <NameComp />
+      {data.name.length <= 0 ? <NameComp /> : null}
+      {data.email.length <= 0 ? <EmailComp /> : null}
+
+      {data.mobile.length <= 0 ? <MobileNoComp /> : null}
+      {data.gender.length <= 0 ? <GenderComp /> : null}
+
+      {data.email.length <= 0 ? <PasswordComp /> : null}
     </div>
   );
 }
--- src/components/name.comp.tsx
+++ src/components/name.comp.tsx
@@ -1,15 +1,44 @@
 import * as React from 'react';
-import './style.css';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  saveName,
+  saveEmail,
+  saveGender,
+  savePassword,
+  saveMobile,
+  getData,
+} from '../features/reducers';
 
 export default function NameComp() {
+  const dispatch = useDispatch();
+
+  const data = useSelector((c) => {
+    console.log(c.show);
+    return c.show;
+  });
+  const [name, setName] = React.useState('');
   return (
     <div>
       <h1>React Redux</h1>
 
       <form action="/action_page.php">
         <label for="fname">Name:</label>
-        <input type="text" id="fname" name="fname" />
+        <input
+          type="text"
+          id="fname"
+          name="fname"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
         <br></br>
+        <button
+          onClick={() => {
+            dispatch(saveName(name));
+          }}
+        >
+          Proceed
+        </button>
       </form>
     </div>
   );
